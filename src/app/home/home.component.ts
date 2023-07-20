7f79dca5b8ae91f207db9c6fe5c81da44fc440c3
duyrua <106798993+BaoDuy313@users.noreply.github.com>
2023-07-20 13:33:40 +0700

    ngforApDung

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,12 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Trang Bảo Duy';
   public age = 15;
-  public traicay = ['Táo','Nho','Cam','Quýt','Ổi'];
+  public traicay = ['Táo', 'Nho', 'Cam', 'Quýt', 'Ổi'];
+  public traicay2 = [
+    { ten: 'Táo', gia: 12, haGia: true },
+    { ten: 'Nho', gia: 3, haGia: true },
+    { ten: 'Cam', gia: -2, haGia: false },
+  ];
   constructor() { }
 
   public ngOnInit(): void {
-    console.log('trai cay = ',this.traicay);
-   }
+    console.log('trai cay = ', this.traicay);
+  }
   public resetName(): void {
     console.log('reset name');
     this.name = '';
